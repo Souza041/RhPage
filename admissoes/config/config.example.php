cf8d021f016735c0a89ffd3a7b4a48a1cd784dc2
Dhionatas Souza <dhiow04@gmail.com>
2026-05-15 15:44:35 -0300

    Ajustes de privacidade de dados

---
+++ admissoes/config/config.example.php
@@ -0,0 +1,13 @@
+<?php
+define('BASE_PATH', realpath(__DIR__ . '/..')); // aponta pra /admissoes
+define('ROOT_PATH', realpath(__DIR__ . '/../..')); // aponta pra /rhpage
+
+define('APP_NAME', 'Solicitações RH');
+define('BASE_URL', '/RHpage/admissoes');
+
+define('DB_HOST', 'dbhost');
+define('DB_NAME', 'bdname');
+define('DB_USER', 'dbuser');
+define('DB_PASS', 'dbpassword');
+
+define('ENV', 'production'); // ou 'dev'
---
+++ admissoes/config/mail.example.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'host' => 'email-ssl.com.br',
+    'porta' => 465,
+    'usuario' => 'chamados@example.com.br',
+    'senha' => 'Ch@example.26',
+    'secure' => 'ssl', // tls ou ssl
+    'from_email' => 'email@example.com.br',
+    'from_nome' => 'Sistema RH Rodobras',
+    'charset' => 'UTF-8',
+
+    // 🔥 MODO TESTE
+    'modo_teste' => true,
+    'email_teste' => 'suporte.ti2@example.com.br'
+];
